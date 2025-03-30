845639fdddf5020db5a9955e30f6c52ad49cb33a
TreatEvlar <kuldeepkumarkumar1987@gmail.com>
2025-03-30 12:50:58 +0530

    hello

---
+++ script.js
@@ -0,0 +1,9 @@
+function openNav(){
+    const open = document.querySelector('.navbar ul')
+    open.classList.add('active')
+}
+
+function navClose(){
+    const close = document.querySelector('.navbar ul')
+    close.classList.remove('active')
+}
